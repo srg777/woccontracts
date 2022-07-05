e2a15e52c7510eee44e451fe0f2a067560734683
srg777 <oracle@ukr.net>
2022-07-05 08:19:13 +0300

    first commit

---
+++ contracts/woclaunchpad.sol
@@ -0,0 +1,260 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./openzeppelin/Ownable.sol";
+import "./openzeppelin/Pausable.sol";
+import "./openzeppelin/IERC20.sol";
+import "./openzeppelin/IERC20Metadata.sol";
+import "./openzeppelin/ReentrancyGuard.sol";
+
+interface Burnable {
+    function burn(uint256 amount) external returns (bool);
+
+    function burnFrom(address account, uint256 amount) external returns (bool);
+}
+
+contract WOCPAD is Ownable, Pausable, ReentrancyGuard {
+    // initial constants
+    address private _coinFromAddress;
+    address private _coinToAddress; //PLC usually
+    uint256 private _p0; //initial price in coinFrom to buy 1 coinTo
+    uint256 private _pmax;
+    uint256 private _dmax;
+
+    uint8 private _coinFromDecimals; //IERC20(_coinFromAddress).decimals()
+    uint8 private _coinToDecimals;
+
+    //state variables
+    uint256 private _ps;
+    uint256 private _ds;
+    uint64 private _countSales;
+
+    bool private _soldout;
+    uint256 private _withdrawed;
+
+    constructor(
+        address aFrom,
+        address aTo,
+        uint256 p0,
+        uint256 pmax,
+        uint256 dmax
+    ) Ownable() Pausable() {
+        require(
+            aFrom != address(0) &&
+                aTo != address(0) &&
+                dmax > 0 &&
+                pmax > p0 &&
+                p0 > 0,
+            "Could not create contract, some init constants are wrong"
+        );
+        _coinFromAddress = aFrom;
+        _coinToAddress = aTo; //PLC usually
+        _p0 = p0; //in Wei
+        _pmax = pmax; //in Wei
+        _dmax = dmax; //in Wei
+
+        _coinFromDecimals = IERC20Metadata(_coinFromAddress).decimals();
+        _coinToDecimals = IERC20Metadata(_coinToAddress).decimals();
+        _ds = 0; //nothing sold yet
+        _withdrawed = 0;
+        _countSales = 0;
+
+        _soldout = false; //not soldout
+
+        if (!paused()) {
+            //initially paused!
+            setPause(true);
+        }
+    }
+
+    /**
+     * deposit supposed to be called only once
+     * this function will invoke coinTo.transferFrom
+     * deposit will move contract from not ready to ready state and from paused to unpased
+     * not ready state means that nothing sold yet i.e. _ds == 0 and balanceOf the contract is 0
+     */
+    function deposit() public onlyOwner {
+        uint256 balance = IERC20(_coinToAddress).balanceOf(address(this));
+        require(
+            !isSoldout() && _ds == 0 && balance == 0,
+            "Contract is already depositet earlier"
+        );
+        IERC20(_coinToAddress).transferFrom(msg.sender, address(this), _dmax);
+        IERC20(_coinToAddress).approve(msg.sender, _dmax);
+        setPause(false);
+    }
+
+    /**
+     * buy some amount of PTC for calculated amound of USDC depending on price change
+     */
+
+    function sell(uint256 ptcAmount) public whenNotPaused nonReentrant {
+        //buy specific amount of PTC
+        require(
+            !isSoldout(),
+            "Sell impossible: Contract shouldn't be in a soldout state"
+        );
+        require(
+            IERC20(_coinToAddress).balanceOf(address(this)) >= ptcAmount,
+            "Sell immpossible: not enough PTC balance"
+        );
+        require(
+            _ds + ptcAmount <= _dmax,
+            "Sell immpossible: PTC limit reached (ds+amount>=dmax)"
+        );
+
+        uint256 oldFromBalance = IERC20(_coinFromAddress).balanceOf(
+            address(this)
+        );
+        uint256 oldToBalance = IERC20(_coinToAddress).balanceOf(address(this));
+
+        uint256 samount = _getS(ptcAmount);
+
+        require(
+            IERC20(_coinFromAddress).balanceOf(msg.sender) >= samount,
+            "Sell imposible, because buyer balance < needed amount"
+        );
+
+        require(
+            IERC20(_coinFromAddress).allowance(msg.sender, address(this)) >=
+                samount,
+            "Sell imposible, because allowance < needed amount"
+        );
+
+        bool success = IERC20(_coinFromAddress).transferFrom(
+            msg.sender,
+            address(this),
+            samount
+        );
+
+        require(success, "Sell went wrong, transferFrom finished with error");
+
+        _ds += ptcAmount;
+        _countSales += 1;
+
+        IERC20(_coinToAddress).transfer(msg.sender, ptcAmount);
+
+        if (_ds == _dmax) {
+            _soldout = true;
+        }
+        /* 
+checking invariance
+first: our newBalance in FROM coin should increase by payed amount sharp
+second: our newBalance in TO coin should decrease for sold amount sharp
+*/
+        assert(
+            IERC20(_coinFromAddress).balanceOf(address(this)) ==
+                oldFromBalance + samount
+        );
+        assert(
+            IERC20(_coinToAddress).balanceOf(address(this)) ==
+                oldToBalance - ptcAmount
+        );
+    }
+
+    /**
+     * Calculating price for specific dx
+     * multiply dx by tg(a)
+     */
+    function _getpx(uint256 dx) private view returns (uint256) {
+        require(
+            _ds + dx >= 0 && _ds + dx <= _dmax,
+            "getpx couldn't calculate price for d more than dmax"
+        );
+        return _p0 + (dx * (_pmax - _p0)) / _dmax;
+    }
+
+    /**
+     * simple approach, mult average price for this range by length of range
+     */
+    function _getS(uint256 dx) private view returns (uint256) {
+        require(
+            _ds + dx >= 0 && _ds + dx <= _dmax,
+            "getS: dx should be in 0 .. dmax-ds range"
+        );
+        uint256 avgPrice = (_getpx(_ds) + _getpx(_ds + dx)) / 2;
+
+        return (dx * avgPrice) / 10**_coinToDecimals;
+    }
+
+    /**
+     * withdraw whole amount of the secont coin
+     */
+    function withdraw() external onlyOwner {
+        uint256 balance = IERC20(_coinFromAddress).balanceOf(address(this));
+        require(balance > 0, "Nothing to withdraw");
+        IERC20(_coinFromAddress).transfer(msg.sender, balance);
+        _withdrawed += balance;
+    }
+
+    /**
+     * burn coins belonged to contract
+     * and if you burn all the coins - change the state to soldout
+     */
+    function burn(uint256 amount) public onlyOwner {
+        require(
+            IERC20(_coinToAddress).balanceOf(address(this)) >= amount,
+            "Burn immpossible: not enough PTC balance"
+        );
+        require(
+            _ds + amount <= _dmax,
+            "Burn immpossible: PTC limit reached (ds+amount>=dmax)"
+        );
+
+        Burnable(_coinToAddress).burn(amount);
+        _ds += amount;
+
+        if (_ds == _dmax) {
+            _soldout = true;
+        }
+    }
+
+    function setPause(bool pause_flag) public onlyOwner {
+        if (pause_flag) {
+            _pause();
+        } else {
+            _unpause();
+        }
+    }
+
+    function isSoldout() public view returns (bool) {
+        return _soldout;
+    }
+
+    function withdrawed() public view onlyOwner returns (uint256) {
+        return _withdrawed;
+    }
+
+    function getp0() public view returns (uint256) {
+        return (_p0);
+    }
+
+    function getpmax() public view returns (uint256) {
+        return (_pmax);
+    }
+
+    function getdmax() public view returns (uint256) {
+        return (_dmax);
+    }
+
+    function getCoinFromAddress() public view returns (address) {
+        return _coinFromAddress;
+    }
+
+    function getCoinToAddress() public view returns (address) {
+        return _coinToAddress;
+    }
+
+    function getds() public view returns (uint256) {
+        return _ds;
+    }
+
+    function getpx(uint256 dx) public view returns (uint256) {
+        return _getpx(dx);
+    }
+
+    function getS(uint256 dx) public view returns (uint256) {
+        return _getS(dx);
+    }
+}
---
+++ contracts/wocptc.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./openzeppelin/ERC20.sol";
+import "./openzeppelin/Ownable.sol";
+import "./openzeppelin/Pausable.sol";
+
+/**
+ * WOCPTC is based on ERC20 standart functionality
+ * but it has some additional features:
+ * ✅ Ownable, i.e. contract has owner how allowed to run some particular featuers
+ * ✅ Pausable, i.e Owner can pause all transfers for some time
+ * ✅ Mintable, i.e. Owner can enable/disable mint function and mint additional coins
+ * ✅ Burnable, i.e. accounts can burn tokens
+ * ✅ Votable, i.e. accounts can delegate their voice to somebody and Owner can receive current Voiting Power of
+ */
+
+struct AddressBalance {
+    address addr;
+    uint256 balance;
+    bool updateflag;
+}
+
+contract WOCPTC is ERC20, Ownable, Pausable {
+    AddressBalance[] internal _snapshot;
+    uint256 internal _snapshotBlock;
+
+    constructor() ERC20("WOC Platinum Coin", "WOCPTC") Ownable() Pausable() {
+        _mint(_msgSender(), 21000000 * 10**decimals());
+    }
+
+    function setPause(bool pause_flag) public onlyOwner {
+        if (pause_flag) {
+            _pause();
+        } else {
+            _unpause();
+        }
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+        require(!paused(), "Contract paused! Transfer not possible");
+
+        if (balanceOf(to) == 0) {
+            _snapshot.push(AddressBalance(to, 0, false));
+        }
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._afterTokenTransfer(from, to, amount);
+
+        if (balanceOf(from) == 0) {
+            bool retcode;
+            uint256 index;
+            (retcode, index) = _indexForAddress(from);
+            if (retcode) {
+                _snapshot[index] = _snapshot[_snapshot.length - 1];
+                _snapshot.pop();
+            }
+        }
+    }
+
+    function mint(address to, uint256 amount) public virtual onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public virtual whenNotPaused returns (bool) {
+        _burn(_msgSender(), amount);
+        return true;
+    }
+
+    function burnFrom(address account, uint256 amount)
+        public
+        virtual
+        whenNotPaused
+        returns (bool)
+    {
+        address spender = _msgSender();
+        require(
+            allowance(account, spender) >= amount,
+            "Cannot burnFrom  more than allowed"
+        );
+        _spendAllowance(account, spender, amount);
+        _burn(account, amount);
+        return true;
+    }
+
+    function getSnapshot()
+        public
+        view
+        onlyOwner
+        returns (AddressBalance[] memory)
+    {
+        return (_snapshot);
+    }
+
+    function getSnapshotBlock() public view onlyOwner returns (uint256) {
+        return (_snapshotBlock);
+    }
+
+    function updateSnapshot() public onlyOwner returns (bool) {
+        _snapshotBlock = block.number;
+
+        for (uint256 i = 0; i < _snapshot.length; i++) {
+            _snapshot[i].balance = balanceOf(_snapshot[i].addr);
+            _snapshot[i].updateflag = true;
+        }
+        return (true);
+    }
+
+    function _indexForAddress(address theAddress)
+        private
+        view
+        returns (bool, uint256)
+    {
+        for (uint256 i = 0; i < _snapshot.length; i++) {
+            if (_snapshot[i].addr == theAddress) {
+                return (true, i);
+            }
+        }
+        return (false, 0);
+    }
+}
